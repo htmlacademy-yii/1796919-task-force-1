c7e2195547644464b9e09a8af6acf163063c8651
reaferon <reaferon@ya.ru>
2021-06-04 10:56:40 +0300

    fix: Планирование – залог успеха, review#1

--- taskforce.sql
+++ taskforce.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-                            `id` int(11) NOT NULL,
+                            `id` int NOT NULL,
                             `title` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -39,25 +39,24 @@ CREATE TABLE `category` (
 --
 
 CREATE TABLE `city` (
-                        `id` int(11) NOT NULL,
+                        `id` int NOT NULL,
                         `title` text NOT NULL,
-                        `latitude` decimal(10,7) DEFAULT NULL,
-                        `longitude` decimal(10,7) DEFAULT NULL
+                        `coordinates` point DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `comment`
+-- Структура таблицы `review`
 --
 
-CREATE TABLE `comment` (
-                           `id` int(11) NOT NULL,
-                           `task_id` int(11) NOT NULL,
-                           `customer_id` int(11) NOT NULL,
-                           `worker_id` int(11) NOT NULL,
+CREATE TABLE `review` (
+                           `id` int NOT NULL,
+                           `task_id` int NOT NULL,
+                           `customer_id` int NOT NULL,
+                           `worker_id` int NOT NULL,
                            `body` text NOT NULL,
-                           `rate` int(1) DEFAULT NULL,
+                           `rate` tinyint DEFAULT NULL,
                            `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -68,9 +67,9 @@ CREATE TABLE `comment` (
 --
 
 CREATE TABLE `favorits` (
-                            `id` int(11) NOT NULL,
-                            `master_user_id` int(11) NOT NULL,
-                            `slave_user_id` int(11) NOT NULL
+                            `id` int NOT NULL,
+                            `master_user_id` int NOT NULL,
+                            `slave_user_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -80,9 +79,9 @@ CREATE TABLE `favorits` (
 --
 
 CREATE TABLE `file_storage` (
-                                `id` int(11) NOT NULL,
-                                `user_id` int(11) DEFAULT NULL,
-                                `task_id` int(11) DEFAULT NULL,
+                                `id` int NOT NULL,
+                                `user_id` int DEFAULT NULL,
+                                `task_id` int DEFAULT NULL,
                                 `path` varchar(1024) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -93,10 +92,12 @@ CREATE TABLE `file_storage` (
 --
 
 CREATE TABLE `message` (
-                           `id` int(11) NOT NULL,
-                           `task_id` int(11) NOT NULL,
-                           `user_id` int(11) NOT NULL,
+                           `id` int NOT NULL,
+                           `task_id` int NOT NULL,
+                           `sender_id` int NOT NULL,
+                           `recipient_id` int NOT NULL,
                            `body` text NOT NULL,
+                           `was_read` tinyint NOT NULL DEFAULT '0',
                            `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -107,11 +108,11 @@ CREATE TABLE `message` (
 --
 
 CREATE TABLE `response` (
-                            `id` int(11) NOT NULL,
-                            `task_id` int(11) NOT NULL,
-                            `worker_id` int(11) NOT NULL,
+                            `id` int NOT NULL,
+                            `task_id` int NOT NULL,
+                            `worker_id` int NOT NULL,
                             `comment` text NOT NULL,
-                            `price` int(11) NOT NULL,
+                            `price` int NOT NULL,
                             `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -122,19 +123,18 @@ CREATE TABLE `response` (
 --
 
 CREATE TABLE `task` (
-                        `id` int(11) NOT NULL,
+                        `id` int NOT NULL,
                         `title` text NOT NULL,
                         `description` text NOT NULL,
-                        `price` int(11) DEFAULT '0',
-                        `category_id` int(11) NOT NULL,
+                        `price` int DEFAULT '0',
+                        `category_id` int NOT NULL,
                         `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         `finish_at` datetime DEFAULT NULL,
                         `status` enum('new','canceled','working','fail','complete') NOT NULL DEFAULT 'new',
-                        `latitude` decimal(10,7) DEFAULT NULL,
-                        `longitude` decimal(10,7) DEFAULT NULL,
-                        `city_id` int(10) UNSIGNED DEFAULT NULL,
-                        `customer_id` int(10) UNSIGNED NOT NULL,
-                        `worker_id` int(10) UNSIGNED DEFAULT NULL
+                        `coordinates` point DEFAULT NULL,
+                        `city_id` int UNSIGNED DEFAULT NULL,
+                        `customer_id` int UNSIGNED NOT NULL,
+                        `worker_id` int UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -144,19 +144,23 @@ CREATE TABLE `task` (
 --
 
 CREATE TABLE `user` (
-                        `id` int(10) UNSIGNED NOT NULL,
+                        `id` int UNSIGNED NOT NULL,
                         `name` varchar(512) NOT NULL,
                         `email` varchar(256) NOT NULL,
                         `password` varchar(32) NOT NULL,
                         `birthday` datetime DEFAULT NULL,
                         `register_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                        `city_id` int(10) UNSIGNED DEFAULT NULL,
+                        `city_id` int UNSIGNED DEFAULT NULL,
                         `avatar` varchar(1024) DEFAULT NULL,
-                        `about` text,
+                        `about` text DEFAULT NULL,
                         `phone` varchar(128) DEFAULT NULL,
-                        `show_profile` tinyint(1) NOT NULL DEFAULT '0',
-                        `show_contacts` tinyint(1) NOT NULL DEFAULT '0',
-                        `notify` tinyint(1) NOT NULL DEFAULT '0'
+                        `skype` varchar(128) DEFAULT NULL,
+                        `telegram` varchar(128) DEFAULT NULL,
+                        `show_profile` tinyint NOT NULL DEFAULT '0',
+                        `show_contacts` tinyint NOT NULL DEFAULT '0',
+                        `notify_message` tinyint NOT NULL DEFAULT '0',
+                        `notify_review` tinyint NOT NULL DEFAULT '0',
+                        `notify_action` tinyint NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -166,8 +170,8 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `user_category` (
-                                 `category_id` int(10) UNSIGNED NOT NULL,
-                                 `user_id` int(10) UNSIGNED NOT NULL
+     `category_id` int UNSIGNED NOT NULL,
+     `user_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -189,7 +193,7 @@ ALTER TABLE `city`
 --
 -- Индексы таблицы `comment`
 --
-ALTER TABLE `comment`
+ALTER TABLE `review`
     ADD PRIMARY KEY (`id`),
   ADD KEY `task_id` (`task_id`),
   ADD KEY `customer_id` (`customer_id`),
@@ -216,7 +220,8 @@ ALTER TABLE `file_storage`
 ALTER TABLE `message`
     ADD PRIMARY KEY (`id`),
   ADD KEY `task_id` (`task_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`sender_id`),
+  ADD KEY `user_id` (`recipient_id`);
 
 --
 -- Индексы таблицы `response`
@@ -259,55 +264,55 @@ ALTER TABLE `user_category`
 -- AUTO_INCREMENT для таблицы `category`
 --
 ALTER TABLE `category`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `city`
 --
 ALTER TABLE `city`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `comment`
 --
 ALTER TABLE `comment`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `favorits`
 --
 ALTER TABLE `favorits`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `file_storage`
 --
 ALTER TABLE `file_storage`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `message`
 --
 ALTER TABLE `message`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `response`
 --
 ALTER TABLE `response`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `task`
 --
 ALTER TABLE `task`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-    MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
